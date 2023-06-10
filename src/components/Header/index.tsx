f46379d47b8a60afb4565a5bfdf979ea4013b294
Ny Nivoarijaona <nynivoarijaona@gmail.com>
2023-06-10 09:39:44 +0300

    responsive header and homepage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { Link } from "gatsby";
 import MaterialSymbolsLinkedCameraOutline from "../Icons/MaterialSymbolsLinkedCameraOutline";
 import Facebook from "../Icons/Facebook";
 import Instagram from "../Icons/Instagram";
 import Email from "../Icons/Email";
+import SolarHamburgerMenuBroken from "../Icons/SolarHamburgerMenuBroken";
+import IconoirCancel from "../Icons/IconoirCancel";
 
-function Header() {
+/* get windows width */
+function getCurrentDimension() {
+  return window.innerWidth;
+}
+
+const Header = () => {
+  const [widthSize, setWidthSize] = useState(1000);
+  const [showMenuMobile, setShowMenuMobile] = useState(false);
+
+  useEffect(() => {
+    const updateDimension = () => {
+      setWidthSize(getCurrentDimension());
+    };
+    updateDimension(); // Set initial value
+
+    // Only add event listener in the browser environment
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", updateDimension);
+
+      return () => {
+        window.removeEventListener("resize", updateDimension);
+      };
+    }
+  }, []);
   return (
     <header>
       <nav>
@@ -14,7 +39,11 @@ function Header() {
           <MaterialSymbolsLinkedCameraOutline />
           Mampi.
         </Link>
-        <ul className="nav-menu">
+        <ul
+          className={`nav-menu ${widthSize < 768 && "mobileActive"} ${
+            showMenuMobile && "active"
+          }`}
+        >
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -36,12 +65,22 @@ function Header() {
             </a>
           </li>
           <li>
-            <Link to="/" className="contact-button">Contact</Link>
+            <Link to="/" className="contact-button">
+              Contact
+            </Link>
           </li>
         </ul>
+        {widthSize < 768 && (
+          <button
+            className="menu-burger"
+            onClick={() => setShowMenuMobile((current) => !current)}
+          >
+            {showMenuMobile ? <IconoirCancel /> : <SolarHamburgerMenuBroken />}
+          </button>
+        )}
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
