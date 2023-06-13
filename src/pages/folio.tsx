f9f766317c46d37d6c689ab4a65a90e44018fb83
Ny Nivoarijaona <nynivoarijaona@gmail.com>
2023-06-13 11:05:10 +0300

    add useffect in folio

--- src/pages/folio.tsx
+++ src/pages/folio.tsx
@@ -5,6 +5,12 @@ import "../styles/folio.scss";
 import GalleryPhotos from "../components/GalleryPhotos";
 
 const FolioPage: React.FC = () => {
+  React.useEffect(() => {
+    var overlay = document.querySelector(".overlay-loading");
+    var bodyTag = document.querySelector("body");
+    overlay?.classList.add("loaded");
+    bodyTag?.classList.remove("hidebody");
+  }, []);
   return (
     <Layout>
       <section className="portfolio-page">
@@ -12,7 +18,9 @@ const FolioPage: React.FC = () => {
           <div className="overlay"></div>
           <div className="home_container">
             <h1 data-aos="fade-down">Mampionona RAKOTOJAONA</h1>
-            <span  data-aos="fade-up" className="sixCaps">PORTFOLIO - GALLERY</span>
+            <span data-aos="fade-up" className="sixCaps">
+              PORTFOLIO - GALLERY
+            </span>
           </div>
         </div>
       </section>
