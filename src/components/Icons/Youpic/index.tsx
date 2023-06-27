d563a3e9f3c6d5eaea128849a06ee824c0cac389
Ny Nivoarijaona <nynivoarijaona@gmail.com>
2023-06-27 22:19:04 +0300

    fix w3c validator

--- src/components/Icons/Youpic/index.tsx
+++ src/components/Icons/Youpic/index.tsx
@@ -3,29 +3,21 @@ import React, { SVGProps } from "react";
 export function Youpic(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      id="Layer2"
-      data-name="Layer 2"
       xmlns="http://www.w3.org/2000/svg"
       width="16.151"
       height="17.272"
       viewBox="0 0 16.151 17.272"
       {...props}
     >
-      <g id="Layer1" data-name="Layer 1" fill="currentColor">
+      <g fill="currentColor">
         <path
-          id="Tracé6"
-          data-name="Tracé 6"
           d="M0,0H4.623V1.943H2.28V15.329H4.623v1.944H0Z"
         />
         <path
-          id="Tracé7"
-          data-name="Tracé 7"
           d="M296.89,15.329h2.484V1.943H296.89V0h4.623V17.272H296.89Z"
           transform="translate(-285.362)"
         />
         <path
-          id="Tracé8"
-          data-name="Tracé 8"
           d="M113.9,144.53h2.226l1.687,3.461,1.687-3.461h2.261l-2.908,5.528v3.374h-2.082l.036-3.374Z"
           transform="translate(-109.755 -139.339)"
         />
